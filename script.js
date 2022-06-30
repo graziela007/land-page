d86a0038e470590cadf8e7bf4fc56848a430c793
graziela007 <grazieladinizdossantos@gmail.com>
2022-06-29 21:47:39 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,36 @@
+const name          = "graziela"
+const sobreNome     = "diniz"
+const data_hora     = "18/07/2005"
+
+let altura          = 1.58
+let numero_sapato   = 35
+let idade           = 16
+
+const celular       = "(18) 996864193"
+let cor_favoritta   = "vermelho"
+
+let qtde_moto       = null
+let qtde_carro      = null
+
+
+qtde_carro = 1
+
+altura = 1.59
+
+celular = "(18) 996864193"
+
+// comparar/validar/verificar Igualdade
+
+nome.trim().toUpperCase() == "GRAZIELA" // false
+
+idade == 17 // true
+
+idade = 16 
+
+idade == 16 // true
+
+(true == true) == (true == false)
+
+// Igaldade escrita
+
+nome === "graziela" 
